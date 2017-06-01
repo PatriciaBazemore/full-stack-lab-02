cd0505b287336bd88a009d3b8c1bcc2a91958b78
PatriciaBazemore <trishbazemore@gmail.com>
2017-05-31 23:06:29 -0400

    tidied up my syntax

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var friends=["Katie", "Anna", "Margaret", "Anna", "Ryan"];
 
 //console.log(friends)
-//console.log(friends[0])
+//console.log(friends(0))
 
 
 
@@ -11,12 +11,13 @@ for (var f = 0; f < friends.length; f++){
     for (var i = 99; i > 0 ; i--) {
         var x = i-1;
 
-        if(i >1){
+      if(i >1){
         console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[f] + " strikes one out, clears it all, "+ x + " lines of code in the file")
-    } else {
+      } 
+        else {
         console.log(i + " line of code in the file, " + i + " line of code; " + friends[f] + " strikes one out, clears it all, " + "no more lines of code in the file")
 
-        }
+        }  
 
 
     }
